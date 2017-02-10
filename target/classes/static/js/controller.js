f6c5ab09ceefc8fa4e57c85144a330cd2d4ac784
rana <masud.it09@gmail.com>
2017-02-10 17:24:14 +0600

    login basic

--- target/classes/static/js/controller.js
+++
@@ -1,9 +0,0 @@
-stepApp.controller('usersController', function($scope) {
-    console.log('click');
-    $scope.headingTitle = "User List";
-});
-
-stepApp.controller('rolesController', function($scope) {
-    console.log('click');
-    $scope.headingTitle = "Roles List";
-});
